a056740870caa91fc7ae25a70a8d5c0ad45555cd
D1atonic <86524918+D1atonic@users.noreply.github.com>
2022-04-11 22:00:48 +0700

    push code

---
+++ Computer/ElectroShop/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using HALAShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HALAShop.Areas.Admin.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        private HALAShopDbContext db = new HALAShopDbContext();
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            ViewBag.CountSlider = db.Sliders.Where(m => m.Status == 1).Count();
+            ViewBag.CountProduct = db.Products.Where(m => m.Status == 1).Count();
+            ViewBag.CountContactDoneReply = db.Contacts.Where(m => m.Flag == 0).Count();
+            ViewBag.CountUser = db.Users.Where(m => m.Status != 0 && m.Access==0).Count();
+            return View();
+        }
+    }
+}
---
+++ Computer/ElectroShop/Models/MProduct.cs
@@ -0,0 +1,33 @@
+﻿    namespace HALAShop.Models
+{
+    using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+    [Table("Products")]
+    public class MProduct
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn nhập tên sản phẩm")]
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn loại sản phẩm")]
+        public int CateID { get; set; }
+        public string Image { get; set; }
+        public string Detail { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập mô tả sản phẩm")]
+        public string MetaKey { get; set; }
+        public string MetaDesc { get; set; }
+        public int Status { get; set; }
+        public DateTime Created_at { get; set; }
+        public int Created_by { get; set; }
+        public DateTime Updated_at { get; set; }
+        public int Updated_by { get; set; }
+    }
+}
